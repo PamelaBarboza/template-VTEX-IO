9ec1cdfea413f65cc1b0d25b27d1ea23a5cf3a37
PamelaBarboza <pamelarabarboza@gmail.com>
2021-02-17 12:06:32 -0300

    learning

--- node/clients/analytics.ts
+++ node/clients/analytics.ts
@@ -1,3 +1,20 @@
-import { AppClient } from '@vtex/api'
 
-export default class Analytics extends AppClient {}
+import { AppClient, InstanceOptions, IOContext } from '@vtex/api'
+
+
+export default class Analytics extends AppClient {
+    constructor(context: IOContext, options?: InstanceOptions) {
+    super('vtex.mocked-analytics@0.x', context, options)
+  }
+
+  
+  public getLiveUsers(): Promise<LiveUsersProduct[]> {
+       return this.http.get('_v/live-products')
+  }
+}
+//slug:un string que representa el ID del producto 
+//liveUsers:un número que es la cantidad de usuarios que visualizan este producto,
+interface LiveUsersProduct {
+  slug: string
+  liveUsers: number
+}
